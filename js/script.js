6df96e99c2e02b07858e80e897f141cc88015fb5
sfaverman <46459931+sfaverman@users.noreply.github.com>
2019-02-07 20:11:58 -0800

    Assignment 1
    Assignment 1

---
+++ js/script.js
@@ -0,0 +1,200 @@
+/*eslint-env browser*/
+//STEP1
+
+/*
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+*/
+
+//STEP2
+
+/*
+5
+"Hello"
+false
+null
+*/
+
+//STEP3
+
+/*
+var a, b, c;
+a = 1;
+b = a + 10; // complex var expression 1
+window.console.log(b);
+c = 2*a + 3*b;  // complex var expression 1
+window.console.log(c); 
+*/
+
+//STEP4
+/*
+var firstName, lastName, strAddress, strCity, strState, zipCode, numYourAge, referralSource, blnMayWeContactYou;
+*/
+
+//STEP5
+/*
+var firstName, lastName;
+var firstName = "Sofia";
+var lastName="Faverman"
+var strAddress = "11774 Windcrest Ln";
+var strCity = "San Diego", strState ="CA", zipCode = 92128;
+var numYourAge = 20, referralSource = "indeed.com"; 
+var blnMayWeContactYou;
+blnMayWeContactYou = false;
+*/
+
+//STEP6
+/*
+var myAddress, myCity, myState, myZipCode;
+myCity = "San Diego";
+myState = "CA";
+myZipCode = 99999;
+myAddress = 11774 + " Windcrest Ln, " + myCity + ' ' + myState + ' ' + myZipCode;
+window.console.log(myAddress);
+*/
+
+//STEP7
+
+/*var blnMayWeContactYou = "May we contact you? " + false;
+window.console.log(blnMayWeContactYou);
+
+var numYourAge = 40 + true;
+window.console.log(numYourAge);*/
+
+//STEP8
+
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+
+//STEP9
+
+/*var b = null;
+window.console.log(b);
+// undefined variable.
+var c;
+window.console.log(c);*/
+
+//STEP10
+
+/*
+var myName = "Sofia";
+window.console.log("variable myName is a " + typeof myName);
+
+var myAge = 20;
+window.console.log("variable myAge is a " + typeof myAge);
+
+var blnHaveChildren = true;
+window.console.log("variable blnHaveChildren is a " + typeof blnHaveChildren);
+
+var myCar = {type:"Mercedes", model:"ES-300", color:"silver"}; 
+window.console.log("variable myCar is a " + typeof myCar);
+
+var myAddress;
+window.console.log("variable myAddress is a " + typeof myAddress);
+*/
+
+//STEP11
+
+/*
+yourName = "Sofia Faverman";
+alert("Hello " + "Sofia Faverman" + ", welcome to the JavaScript class!");
+*/
+
+//STEP12
+
+/*
+yourName = "Sofia Faverman";
+alert("Hello " + yourName + ", welcome to the JavaScript class!");
+*/
+//STEP13
+
+/*yourName = prompt("Please enter your name");
+
+alert("Hello " + yourName + ", welcome to the JavaScript class!");*/
+
+//STEP14
+
+/*alert("Hello Sofia Faverman.\nWelcome to the JavaScript class!");*/
+
+//STEP15
+
+/*yourName = prompt("Please enter your name");
+alert("Hello " + yourName + ".\nWelcome to the JavaScript class!");*/
+
+//STEP16
+
+/*yourName = prompt("Please enter your name");
+yourClass = prompt("Please enter your class");
+alert("Hello " + yourName + ".\nWelcome to the " + yourClass + " class!");*/
+
+//STEP17
+/*var x = 10;
+var y = 20;
+window.console.log("Sum = " + (x+y) );*/
+//STEP18
+
+/*
+var x = 20;
+x +=20;
+window.console.log("x = " + x );
+*/
+
+//STEP19
+
+/*
+var x = 20;
+x *=5;
+window.console.log("x = " + x );
+*/
+
+//STEP20
+
+/*var x = 20%3;
+x /=1;
+window.console.log("x = " + x );*/
+
+//STEP21
+
+/*var x = 3, y = 7;
+var z = x < y;  
+window.console.log("x < y " + z);*/
+
+//STEP22
+
+/*var myName = null;
+var validateName = typeof myName;
+
+window.console.log(validateName == "string");*/
+
+//STEP23
+
+/*
+var widget = new Object();
+
+window.console.log(typeof widget);
+*/
+
+
+//STEP24
+
+/*var widget = new Object();
+
+if (widget instanceof Object) {
+ window.console.log("widget is an object");   
+} else {
+ window.console.log("widget is not an object");  
+}; */
+
+//STEP25
+
+/*var widget = new Object();
+
+var evaluate =  !widget instanceof Object;
+window.console.log("evaluate = " + evaluate);   
+  */
